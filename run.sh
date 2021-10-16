0dca22a6e972be31d7e463018a527c475825b475
Taleh Ibrahimli <talehsmail@gmail.com>
2021-10-16 18:47:13 +0400

    minor fixes

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 pkill download-delegator
+export PATH=$PATH:/root/goroot/bin
+cd /root/download-delegator
+git pull
 go build
 nohup ./download-delegator config.toml &> /var/log/download-delegator.log &
